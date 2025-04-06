0721b89f9ac823da24a39ff2f871286fdb2d8fd0
Praveen Kumar Mahto <74883304+mahto-praveen@users.noreply.github.com>
2025-04-06 23:37:53 +0530

    Create Check If N and Its Double Exist.java

---
+++ Check If N and Its Double Exist.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> s = new HashSet<>();
+        for( int i:arr){
+            if(s.contains(i*2) || (s.contains(i/2) && i%2==0)){
+                return true;
+            }
+            s.add(i);
+        }
+        return false;
+    }
+}
