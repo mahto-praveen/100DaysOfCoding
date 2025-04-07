7532839fc63c482a0382019430565f466476e564
Praveen Kumar Mahto <74883304+mahto-praveen@users.noreply.github.com>
2025-04-07 23:01:22 +0530

    Create Clear Digits.java

---
+++ Clear Digits.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public String clearDigits(String s) 
+  {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) 
+        {
+            if (Character.isDigit(c)) 
+            {
+                if (res.length() > 0) 
+                {
+                    res.deleteCharAt(res.length() - 1); // Remove last char
+                }
+            } else 
+            {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
