51bb2d44e13e74e8bf34761d3bd479a56ec22f4c
Praveen Kumar Mahto <74883304+mahto-praveen@users.noreply.github.com>
2025-04-23 22:11:55 +0530

    Create count largest group.java

---
+++ count largest group.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int countLargestGroup(int n) 
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxSize = 0;
+
+        for (int i = 1; i <= n; i++) 
+        {
+            int sum = digitSum(i);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+            maxSize = Math.max(maxSize, map.get(sum));
+        }
+
+        int count = 0;
+        for (int value : map.values()) 
+        {
+            if (value == maxSize) 
+            {
+                count++;
+            }
+        }
+
+        return count;        
+    }
+    private int digitSum(int num) 
+    {
+        int sum = 0;
+        while (num > 0) 
+        {
+            sum += num % 10;   
+            num /= 10;         
+        }
+        return sum;
+    }
+}
